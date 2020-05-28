6f2de121fdacaf4b538a71593780930858b1b12a
PowerChen <powerting@163.com>
2020-05-28 17:34:20 +0800

    update

--- java/spweb/src/main/java/com/powerting/spweb/controller/HiveJdbcController.java
+++ java/spweb/src/main/java/com/powerting/spweb/controller/HiveJdbcController.java
@@ -1,27 +1,22 @@
 package com.powerting.spweb.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 使用 DataSource 操作 Hive
  */
 @RestController
-@RequestMapping("/hive")
+@RequestMapping("/api")
 public class HiveJdbcController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HiveJdbcController.class);
+//    private static final Logger logger = LoggerFactory.getLogger(HiveJdbcController.class);
 
     @Autowired
     @Qualifier("hiveDruidDataSource")
@@ -32,109 +27,14 @@ public class HiveJdbcController {
     DataSource jdbcDataSource;
 
     /**
-     * 通用自己定义传入Hive Sql
-     * opTYpe: select,show, describe
-     */
-    @RequestMapping("/jdbc")
-    public List<String> doHiveJdbc(String opType, String sql) throws SQLException {
-        List<String> list = new ArrayList<String>();
-        String result = null;
-//         Statement statement = jdbcDataSource.getConnection().createStatement();
-        Statement statement = druidDataSource.getConnection().createStatement();
-        ResultSet res;
-        logger.info("opType: " + opType+" sql:"+sql);
-        switch (opType){
-            case "select":
-                res = statement.executeQuery(sql);
-                int count = res.getMetaData().getColumnCount();
-                String str = null;
-                while (res.next()) {
-                    str = "";
-                    for (int i = 1; i < count; i++) {
-                        str += res.getString(i) + " ";
-                    }
-                    str += res.getString(count);
-                    logger.info(str);
-                    list.add(str);
-                }
-                break;
-            case "show":
-                res = statement.executeQuery(sql);
-                while (res.next()) {
-                    list.add(res.getString(1));
-                }
-                break;
-            case "describe":
-                res = statement.executeQuery(sql);
-                while (res.next()) {
-                    list.add(res.getString(1));
-                }
-                break;
-            default:
-                logger.info("unknow opType: " + opType);
-                break;
-
-        }
-        return list;
-    }
-
-    /**
      * 列举当前Hive库中的所有数据表
+     * @return return
      */
-    @RequestMapping("/table/list")
-    public List<String> listAllTables() throws SQLException {
-        List<String> list = new ArrayList<String>();
-//         Statement statement = jdbcDataSource.getConnection().createStatement();
-        Statement statement = druidDataSource.getConnection().createStatement();
-        String sql = "show tables";
-        logger.info("Running: " + sql);
-        ResultSet res = statement.executeQuery(sql);
-        while (res.next()) {
-            list.add(res.getString(1));
-        }
-        return list;
-    }
-
-    /**
-     * 查询Hive库中的某张数据表字段信息
-     */
-    @RequestMapping("/table/describe")
-    public List<String> describeTable(String tableName) throws SQLException {
-        List<String> list = new ArrayList<String>();
-        // Statement statement = jdbcDataSource.getConnection().createStatement();
-        Statement statement = druidDataSource.getConnection().createStatement();
-        String sql = "describe " + tableName;
-        logger.info("Running: " + sql);
-        ResultSet res = statement.executeQuery(sql);
-        while (res.next()) {
-            list.add(res.getString(1));
-        }
-        return list;
-    }
-
-    /**
-     * 查询指定tableName表中的数据
-     */
-    @RequestMapping("/table/select")
-    public List<String> selectFromTable(String tableName) throws SQLException {
-        // Statement statement = jdbcDataSource.getConnection().createStatement();
-        Statement statement = druidDataSource.getConnection().createStatement();
-        String sql = "select * from " + tableName;
-        logger.info("Running: " + sql);
-        ResultSet res = statement.executeQuery(sql);
-        List<String> list = new ArrayList<String>();
-        int count = res.getMetaData().getColumnCount();
-        String str = null;
-        while (res.next()) {
-            str = "";
-            for (int i = 1; i < count; i++) {
-                str += res.getString(i) + " ";
-            }
-            str += res.getString(count);
-            logger.info(str);
-            list.add(str);
-        }
-        return list;
+    @RequestMapping("/test/list")
+    public String listAllTables()  {
+        String result = "Running test list";
+//        logger.info(result);
+        return result;
     }
 
 }
--- java/spweb/src/main/java/com/powerting/spweb/controller/HiveJdbcTemplateController.java
+++ java/spweb/src/main/java/com/powerting/spweb/controller/HiveJdbcTemplateController.java
@@ -1,7 +1,7 @@
 package com.powerting.spweb.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataAccessException;
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
  * 使用 JdbcTemplate 操作 Hive
  */
 @RestController
-@RequestMapping("/hive2")
+@RequestMapping("/api2")
 public class HiveJdbcTemplateController {
 
-    private static final Logger logger = LoggerFactory.getLogger(HiveJdbcTemplateController.class);
+//    private static final Logger logger = LoggerFactory.getLogger(HiveJdbcTemplateController.class);
 
     @Autowired
     @Qualifier("hiveDruidTemplate")
@@ -32,10 +32,10 @@ public class HiveJdbcTemplateController {
      * 通用自己定义传入Hive Sql
      * opTYpe:  create, load, delete
      */
-    @RequestMapping("/jdbc")
+    @RequestMapping("/diy")
     public String doHive2Jdbc(String opType, String sql)  {
         String result = null;
-        logger.info("opType: " + opType+" sql:"+sql);
+//        logger.info("opType: " + opType+" sql:"+sql);
         if(opType != null && sql != null){
             switch (opType){
                 case "create":
@@ -45,7 +45,7 @@ public class HiveJdbcTemplateController {
                         hiveDruidTemplate.execute(hsql.toString());
                     } catch (DataAccessException dae) {
                         result = "Create table encounter an error: " + dae.getMessage();
-                        logger.error(result);
+//                        logger.error(result);
                     }
                     break;
                 case "load":
@@ -55,7 +55,7 @@ public class HiveJdbcTemplateController {
                         hiveDruidTemplate.execute(sql);
                     } catch (DataAccessException dae) {
                         result = "Load data into table encounter an error: " + dae.getMessage();
-                        logger.error(result);
+//                        logger.error(result);
                     }
                     break;
                 case "delete":
@@ -66,11 +66,11 @@ public class HiveJdbcTemplateController {
                         hiveDruidTemplate.execute(sql);
                     } catch (DataAccessException dae) {
                         result = "Drop table encounter an error: " + dae.getMessage();
-                        logger.error(result);
+//                        logger.error(result);
                     }
                     break;
                 default:
-                    logger.info("unknow opType: " + opType);
+//                    logger.info("unknow opType: " + opType);
                     break;
             }
         }else{
@@ -83,60 +83,10 @@ public class HiveJdbcTemplateController {
     /**
      * 示例：创建新表
      */
-    @RequestMapping("/table/create")
+    @RequestMapping("/test/create")
     public String createTable() {
-        StringBuffer sql = new StringBuffer("CREATE TABLE IF NOT EXISTS ");
-        sql.append("user_test");
-        sql.append("(user_num BIGINT, user_name STRING, user_gender STRING, user_age INT)");
-        sql.append("ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' "); // 定义分隔符
-        sql.append("STORED AS TEXTFILE"); // 作为文本存储
-
-        logger.info("Running: " + sql);
         String result = "Create table successfully...";
-        try {
-            // hiveJdbcTemplate.execute(sql.toString());
-            hiveDruidTemplate.execute(sql.toString());
-        } catch (DataAccessException dae) {
-            result = "Create table encounter an error: " + dae.getMessage();
-            logger.error(result);
-        }
-        return result;
-
-    }
-
-    /**
-     * 示例：将Hive服务器本地文档中的数据加载到Hive表中
-     */
-    @RequestMapping("/table/load")
-    public String loadIntoTable() {
-        String filepath = "/home/dxp/testfile";
-        String sql = "load data local inpath '" + filepath + "' into table user_test";
-        String result = "Load data into table successfully...";
-        try {
-            // hiveJdbcTemplate.execute(sql);
-            hiveDruidTemplate.execute(sql);
-        } catch (DataAccessException dae) {
-            result = "Load data into table encounter an error: " + dae.getMessage();
-            logger.error(result);
-        }
         return result;
     }
 
-    /**
-     * 示例：删除表
-     */
-    @RequestMapping("/table/delete")
-    public String delete(String tableName) {
-        String sql = "DROP TABLE IF EXISTS " + tableName;
-        String result = "Drop table successfully...";
-        logger.info("Running: " + sql);
-        try {
-            // hiveJdbcTemplate.execute(sql);
-            hiveDruidTemplate.execute(sql);
-        } catch (DataAccessException dae) {
-            result = "Drop table encounter an error: " + dae.getMessage();
-            logger.error(result);
-        }
-        return result;
-    }
 }
